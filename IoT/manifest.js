db579c1cea0ec9a29010a9283a851c76ad74114d
duydl <duy.dole.00ece@gmail.com>
2026-02-21 18:32:07 +0100

    Init

---
+++ IoT/manifest.js
@@ -0,0 +1,105 @@
+window.deckManifest = {
+    "name": "IoT",
+    "deckPath": "deck.json",
+    "notesPath": "notes.json",
+    "notesHtmlPath": "notes.html",
+    "children": [
+        {
+            "name": "01",
+            "deckPath": "01/deck.json",
+            "notesPath": "01/notes.json",
+            "notesHtmlPath": "01/notes.html",
+            "children": [],
+            "modelsPath": "01/models.json"
+        },
+        {
+            "name": "02",
+            "deckPath": "02/deck.json",
+            "notesPath": "02/notes.json",
+            "notesHtmlPath": "02/notes.html",
+            "children": [],
+            "modelsPath": "02/models.json"
+        },
+        {
+            "name": "03",
+            "deckPath": "03/deck.json",
+            "notesPath": "03/notes.json",
+            "notesHtmlPath": "03/notes.html",
+            "children": [],
+            "modelsPath": "03/models.json"
+        },
+        {
+            "name": "04",
+            "deckPath": "04/deck.json",
+            "notesPath": "04/notes.json",
+            "notesHtmlPath": "04/notes.html",
+            "children": [],
+            "modelsPath": "04/models.json"
+        },
+        {
+            "name": "05",
+            "deckPath": "05/deck.json",
+            "notesPath": "05/notes.json",
+            "notesHtmlPath": "05/notes.html",
+            "children": [],
+            "modelsPath": "05/models.json"
+        },
+        {
+            "name": "06",
+            "deckPath": "06/deck.json",
+            "notesPath": "06/notes.json",
+            "notesHtmlPath": "06/notes.html",
+            "children": [],
+            "modelsPath": "06/models.json"
+        },
+        {
+            "name": "07",
+            "deckPath": "07/deck.json",
+            "notesPath": "07/notes.json",
+            "notesHtmlPath": "07/notes.html",
+            "children": [],
+            "modelsPath": "07/models.json"
+        },
+        {
+            "name": "08",
+            "deckPath": "08/deck.json",
+            "notesPath": "08/notes.json",
+            "notesHtmlPath": "08/notes.html",
+            "children": [],
+            "modelsPath": "08/models.json"
+        },
+        {
+            "name": "09",
+            "deckPath": "09/deck.json",
+            "notesPath": "09/notes.json",
+            "notesHtmlPath": "09/notes.html",
+            "children": [],
+            "modelsPath": "09/models.json"
+        },
+        {
+            "name": "10",
+            "deckPath": "10/deck.json",
+            "notesPath": "10/notes.json",
+            "notesHtmlPath": "10/notes.html",
+            "children": [],
+            "modelsPath": "10/models.json"
+        },
+        {
+            "name": "11",
+            "deckPath": "11/deck.json",
+            "notesPath": "11/notes.json",
+            "notesHtmlPath": "11/notes.html",
+            "children": [],
+            "modelsPath": "11/models.json"
+        },
+        {
+            "name": "exam",
+            "deckPath": "exam/deck.json",
+            "notesPath": "exam/notes.json",
+            "notesHtmlPath": "exam/notes.html",
+            "children": [],
+            "modelsPath": "exam/models.json"
+        }
+    ],
+    "modelsPath": "models.json"
+};
